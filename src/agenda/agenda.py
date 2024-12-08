34ac21a90f32f0fd72bc230159578d186d935d69
Oni <VladOniscenko@outlook.com>
2024-12-08 14:26:07 +0100

    implemented create function from crud

--- src/agenda/agenda.py
+++ src/agenda/agenda.py
@@ -1,6 +1,23 @@
+from datetime import datetime, timedelta
+from .task_manager import TaskManager
+from .task import Task
+
 class Agenda:
     def __init__(self):
-        print("Agenda initialized")
+        self.task_manager = TaskManager()
 
-    def display(self):
+    def create_task(self, title: str, description: str, due_date: datetime | None = None, status: str = 'pending', priority: int = 1):
+        # create a task
+        task = Task(title, description, due_date, status, priority)
+
+        # add task to task manager
+        self.task_manager.add_task(task)
+
+    def read_task(self):
+        pass
+
+    def update_task(self):
+        pass
+
+    def delete_task(self):
         pass
