38e354f8219f2acd7bf05d41f871a7b99ed3b00e
Oni <VladOniscenko@outlook.com>
2024-12-08 15:13:13 +0100

    UI logic

--- main.py
+++ main.py
@@ -5,8 +5,46 @@ import os
 
 def main():
     agenda = Agenda()
-    agenda.create_task('vlad', 'ahahhaha')
-    print(agenda.task_manager)
+
+    box_w = 20
+
+    methods = {
+        1: agenda.create_task
+    }
+
+    actions = [
+        '1: Create task',
+        # '2: Read task',
+        # '3: Update task',
+        # '4: Delete task'
+    ]
+
+    inputs = {
+        1: ['title', 'description', 'due date (01-01-2002 00:00)']
+    }
+
+    while True:
+        print('+' + '-' * box_w + '+')
+        print('|' + 'Actions:'.center(box_w) + '|')
+        print('+' + '-' * box_w + '+')
+
+        for act in actions:
+            print('|' + f'{act}'.center(box_w) + '|')
+            print('+' + '-' * box_w + '+')
+
+        try:
+            action = int(input('Select action: '))
+        except ValueError as e:
+            print(f'Invalid input:')
+            continue
+
+        func = methods.get(action)
+        if not func:
+            print('Action not found!')
+            continue
+
+        required_inputs = inputs.get(action)
+        func(*[input(f'Input {x}: ') for x in required_inputs])
 
 if __name__ == '__main__':
     main()
