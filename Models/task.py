07bf27e86c46e7d794df12e8f126f5046989259b
vlad_mbp <vladoniscenko@outlook.com>
2024-12-18 15:00:49 +0100

    task attributes

---
+++ Models/task.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+class Task:
+    __name: str
+    __description: str
+    __priority: str
+    __date: datetime
+    __status: str
+    __status: tuple[str]
+    __STATUSES: tuple[str, ...] = ("Pending", "In Progress", "Completed", "On Hold", "Cancelled")
+    __HIDDEN: tuple[str, ...] = ()
+
+    def __init__(self, name: str, description: str, date: datetime = datetime.now(), priority: str = 1, status: str = "Pending", identifier: int = None):
+        self.__id = identifier
+        self.__name = name
+        self.__description = description
+        self.__priority = priority
+        self.__date = date
+        self.__status = status
+
+    # return private attributes
+    def __getattr__(self, attr):
+        private_attr = f"_{self.__class__.__name__}__{attr}"
+
+        # check if attr isset and not in
+        if private_attr in self.__dict__ and attr not in self.__HIDDEN:
+            return self.__dict__[private_attr]
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{attr}'")
+
+    # represent instance
+    def __repr__(self) -> str:
+        return "{}({})".format(type(self).__name__, ", ".join([f"{key}={value!r}" for key, value in self.__dict__.items()]))
